f5cb684d8bf01c163b7dbd03182e8d50a6c65825
jason_yao <jason_yao@htc.com>
2013-11-23 21:34:45 +0800

    update

---
+++ example/1123/call_by_value.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void swap(int a, int b)
+{
+    printf("swap[+] a=%d, b=%d\n",a,b);
+    int temp = a;
+    a = b;
+    b = temp;
+    printf("swap[-] a=%d, b=%d\n",a,b);
+}
+
+int main(int argc, char *argv[])
+{
+
+    int a, b;
+
+    a = 50;
+    b = 100;
+    printf("a=%d, b=%d\n",a,b);
+    swap(a,b);
+    printf("a=%d, b=%d\n",a,b);
+
+    return 0;
+}
