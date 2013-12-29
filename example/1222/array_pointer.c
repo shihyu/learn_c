5ac821fcd7e0bfed27605cb555ebf0444ee8c156
jason_yao <jason_yao@htc.com>
2013-12-29 11:52:06 +0800

    update

--- example/1222/array_pointer.c
+++ example/1222/array_pointer.c
@@ -6,6 +6,10 @@ int main(int argc, char *argv[])
     char (*b)[10] = a; // b 是個指標, 64bit 是8byte
     char *c[2] = {"Hello", "world"}; // 16 byte  
     char d[100] = "egergergergewrgewrgjeiorgjoierjgoiergjoiejgoierjgoijeorgjowjerg"; 
+    char *e = "c";
+    char *f = "hello";
+    char g[100000] = "abcdefghij";
+    int i;
 
     printf("%ld\n",sizeof(a));
     printf("%ld\n",sizeof(b));
@@ -31,6 +35,27 @@ int main(int argc, char *argv[])
 
     printf("%s\n",c[0]);
 
+    printf("%c\n",*(e+0));  // 相當於 *e
+    printf("%c\n",*(e+1));
+    printf("%s\n",f);
+    printf("%c\n",*f);
+    printf("%c\n",*(f+4));
+
+    c[0] = e; // 相當於 *c = e , *(c+0) = e
+    c[1] = f; //  相當於 *(c+1) = f
+    printf("%s\n", c[0]);
+    printf("%s\n", c[1]);
+
+    printf("%c\n",*(g+8));
+    printf("%c\n",*(g+9));
+
+
+    for (i = 0; i < 100000; ++i) {
+        printf("%p, i=%d\n",g +i, i);
+        g[i] = 'y';
+    }
+    
+    printf("%ld\n",sizeof(g));
 
 
     return 0;
---
+++ example/1229/function.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+// decla
+void test();
+int test1();
+float test2();
+double test3();
+char test4();
+char* test5();
+int* test6();
+
+
+int g;
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int main(int argc, char* argv[]) {
+
+    printf("%d\n", sum(10,50));
+    test();
+    printf("%d\n", g);
+    return 0;
+}
+
+
+// implement
+void test() {
+
+    g = 100;
+    
+}
+
+int test1() {
+    return 0;
+}
+
+float test2() {
+    return 0;
+}
+
+double test3() {
+    return 0.0f;
+}
+
+char test4() {
+    return 0;
+}
+
+char* test5() {
+    return 0;
+}
+
+int* test6() {
+    return 0;
+}
