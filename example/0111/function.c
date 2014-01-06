49a5b6312e476b1d5e104e699283bb981178d5d5
jason_yao <jason_yao@htc.com>
2014-01-06 19:22:03 +0800

    update

---
+++ example/0111/function.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void test(void);
+int test1(void);
+int test2(int);
+int test3(int, int);
+float test4(float, float);
+void test5(int a[]);  // void test5(int *a)
+void test6(float a[]);  // void test5(int *a)
+void* my_malloc (size_t size);
+
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
