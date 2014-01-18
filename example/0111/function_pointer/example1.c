4614d52999bd6f31b05a46efd7dccf039641ecfa
jason_yao <jason_yao@htc.com>
2014-01-18 20:23:33 +0800

    update

--- example/0111/function_pointer/example1.c
+++ example/0111/function_pointer/example1.c
@@ -2,7 +2,6 @@
 
 int add(int a, int b)
 {
-
     return a + b;
 }
 
@@ -16,25 +15,17 @@ void func2(void) {
 
 int main(void) {
     void (*f)(void) = aaa1;
-
-    int (*p)(int,int)= add;
-
+    int (*p)(int, int)= add;
 
     printf("%d\n", p(10,30));
     printf("%d\n", add(10,30));
-
-
-
 
     // f = func1; // f指向func1
     (*f)();  // 呼叫f, 也就等於呼叫func1
     // func1();
 
-
-
-
-
     f = func2; // f指向func2
     f();     // 呼叫f, 也就等於呼叫func2, 直接寫f()也行.
+
     return 0;
 }
