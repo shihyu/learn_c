b1e6bc75163addc141ecf410f2811c5349eb25d0
jason_yao <jason_yao@htc.com>
2014-03-22 14:17:17 +0800

    update

---
+++ example/0322/atoi.c
@@ -0,0 +1,32 @@
+// gcc -Wall -g atoi.c -o atoi -lm
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+long long my_atoi(char* s) {
+    int i, len;
+    long long sum = 0;
+
+    len = strlen(s) - 1;
+    printf("len=%d\n",len);
+
+    for (i = 0; i < len; ++i) {
+        sum += (s[i] - '0') * (long long)pow(10.0, (double)(len - i - 1));
+    }
+
+    return sum;
+}
+
+int main() {
+    long long value;
+    char buffer[256];
+
+    printf("Enter a number: ");
+    fgets(buffer, 256, stdin);
+    value = my_atoi(buffer);
+
+    printf("\nThe value entered is %lld.\n", value);
+    return 0;
+}
