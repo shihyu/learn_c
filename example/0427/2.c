48a0bf3c3a47037a2b839b58e3a9b602be377ff9
jason_yao <jason_yao@htc.com>
2014-05-02 20:55:35 +0800

    update

---
+++ example/0427/2.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define RANGE 100
+#define M 5
+#define N 5
+
+void show(int row, int col, int ar[row][col]) {
+    int i, j;
+
+    for (i = 0; i < row; ++i) {
+        for (j = 0; j < col; ++j) {
+            printf("%d ", ar[i][j]);
+        }
+
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int main(int argc, char const *argv[])
+{
+    int matrix_a[M][N];
+    int matrix_b[M][N];
+    int tmp;
+
+    srand(time(NULL));
+    
+    for (int i = 0; i < M; ++i) {
+        for (int j = 0; j < N; ++j) {
+            matrix_a[i][j] = rand() % RANGE;
+            matrix_b[i][j] = rand() % RANGE;
+        }
+    }
+
+    show(M, N, matrix_a);
+    show(M, N, matrix_b);
+
+    for (int i = 0; i < M; ++i) {
+        for (int j = 0; j < N; ++j) {
+            tmp = matrix_a[i][j];
+            matrix_a[i][j] = matrix_b[i][j];
+            matrix_b[i][j] = tmp;
+        }
+    }
+
+    show(M, N, matrix_a);
+    show(M, N, matrix_b);
+
+    return 0;
+}
+
