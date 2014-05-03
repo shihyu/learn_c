0dd543db27118e73edd60bbb6f54c554e24b187b
jason_yao <jason_yao@htc.com>
2014-05-04 00:20:44 +0800

    update

--- example/library/Shared_library/main.c
+++ example/library/Shared_library/main.c
@@ -3,7 +3,9 @@
 #include "mul.h"
 
 int main(void) {
+    int (*fp)(int,int) = add; 
     printf("%d\n", add(10, 20));
     printf("%d\n", mul(10, 20));
+    printf("\nfp address=%p\n",*(void**)&fp);
     return 0;
 }
