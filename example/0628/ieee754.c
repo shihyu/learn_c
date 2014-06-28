c0009e7e45f892e574b326a09f798a0c87c76938
jason_yao <jason_yao@htc.com>
2014-06-28 16:42:44 +0800

    update

--- example/0628/ieee754.c
+++ example/0628/ieee754.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int i = 0xc15a0000; 
-    float f = -13.625; // 1 1000 0010 10110100000000000000000
-    int _i = -1051066368;
+    int i = 0xc15a0000;  // 1 1000 0010 10110100000000000000000
+    float f = -13.625;  // 1 1000 0010 10110100000000000000000
+    int _i = -1051066368;  // 1 1000 0010 10110100000000000000000
 
     // c15a0000 = 1 1000 0010 10110100000000000000000   IEEE 754
     printf("0x%08X\n", *(unsigned int*)&f);
     printf("0x%08X\n", *(unsigned int*)&i);
-    printf("0x%08X\n", *(unsigned int*)&_i);
+    printf("0x%08X\n\n", *(unsigned int*)&_i);
 
     printf("f=%f\n",f);
     printf("i =%f\n",i);
-    printf("_i=%f\n",i);
+    printf("_i=%f\n\n",_i);
+
+    printf("%d\n",*(int*)&f);
 
     return 0;
 }
