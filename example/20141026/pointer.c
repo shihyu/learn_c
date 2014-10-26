ef132b8f37ecad63c666274d5910a1ede878666a
jason_yao <jason_yao@htc.com>
2014-10-26 10:24:45 +0800

    update

---
+++ example/20141026/pointer.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void)
+{
+    
+    int i ,j;
+    int a = 100;
+
+    int one_dim_array[5] = {10,20,30,40,50};
+
+    int two_dim_array[3][2] = {{10,20},
+                               {30,40},
+                               {50,60}};
+
+    int *pa = &a;
+    int *parray = one_dim_array;
+    int (*pparray)[2] = two_dim_array;
+
+    printf("%d\n",*pa);
+
+
+    for (i = 0; i < 5; ++i) {
+        printf("parray[%d] = %d\n",i, parray[i]);
+    }
+
+    printf("\n\n");
+
+    for (i = 0; i < 3; ++i) {
+        for (j = 0; j < 2; ++j) {
+            printf("pparray[%d][%d]=%d\n",i,j,pparray[i][j]);
+        }
+    }
+
+    return 0;
+}
