cb567194d5d268b39293b66e2137dcfe93d32422
jasonblog <pythontcl@gmail.com>
2014-11-29 03:23:31 +0800

    update

---
+++ example/20141130/glibc_using_gdb.c
@@ -0,0 +1,23 @@
+/*gcc -g glibc_using_gdb.c -o glibc_using_gdb */
+
+#include <stdio.h>                                                        
+ 
+int main(void){
+    int num;
+
+    puts("Please enter a num: ");
+    scanf("%d", &num);
+
+    printf("%x\n",num);
+ 
+    if (num == 0)
+        puts("The number you entered is 0 ");
+    else if (num >= 0)
+        puts("The number you entered is Positive ");
+    else if (num <= 0 )
+        puts("The number you entered is Negative ");
+    else
+        puts("Unexpected Errors!");
+ 
+    return 0;
+}
