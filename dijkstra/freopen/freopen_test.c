5ce33f82a95302ff6a7aec45cd11504c3d89c8fc
gigigigi <gigigigi>
2015-05-23 13:06:33 +0800

    update

--- dijkstra/freopen/freopen_test.c
+++ dijkstra/freopen/freopen_test.c
@@ -5,13 +5,19 @@ int main()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
+    scanf("%d",&x);
+    scanf("%d",&y);
+
+    printf("%d\n",x);
+    printf("%d\n",y);
+
     while (scanf("%d%d%d", &item, &x, &y) == 3) {
         printf("%d %d %d %d ", item, x, y, x + y);
 
-        if (x + y < 140) {
-            printf("不合格\n");
+        if (x + y < 100) {
+            printf("距離 < 100 \n");
         } else {
-            printf("合格\n");
+            printf("距離 > 100 \n");
         }
     }
 
